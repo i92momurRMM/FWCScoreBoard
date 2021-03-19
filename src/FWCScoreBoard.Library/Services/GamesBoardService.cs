b51d06506521624f2e0fb40b9b5b3f8ccdf61392
Rafael Montero Muñoz <rmontero@net4things.com>
2021-03-19 09:22:23 +0100

    Added GamesBoard service and created StartGame method

---
+++ src/FWCScoreBoard.Library/Services/GamesBoardService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FWCScoreBoard.Library.Domain;
+
+namespace FWCScoreBoard.Library.Services
+{
+	public class GamesBoardService
+	{
+		public GamesBoardService()
+		{
+
+		}
+
+		public Guid StartGame(string homeTeam, string awayTeam)
+		{
+			var game = new Game ( Guid.NewGuid() );
+			game.AddHomeTeam(homeTeam);
+			game.AddAwayTeam(awayTeam);
+
+			return game.Id;
+		}
+	}
+}
+
---
+++ test/FWCScoreBoard.Library.UnitTests/Services/StartGameTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using FWCScoreBoard.Library.Services;
+using FWCScoreBoard.Library.UnitTests.TestSupport;
+
+namespace FWCScoreBoard.Library.UnitTests.Services
+{
+	public static class StartGameTests
+	{
+		public class Given_A_Game_When_Adding_Valid_Teams
+			: Given_When_Then_Test
+		{
+			private GamesBoardService _sut;
+			private string _homeTeam;
+			private string _awayTeam;
+			private Guid _startedGameId;
+
+			protected override void Given()
+			{
+				_homeTeam = "Mexico";
+				_awayTeam = "Canada";
+
+				_sut = new GamesBoardService();
+			}
+
+			protected override void When()
+			{
+				_startedGameId = _sut.StartGame(_homeTeam, _awayTeam);
+			}
+
+			[Fact]
+			public void Then_It_Should_Return_A_Valid_Id()
+			{
+				_startedGameId.Should().NotBeEmpty();
+			}
+		}
+	}
+}
